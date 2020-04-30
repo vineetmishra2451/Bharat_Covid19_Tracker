d7ed5c00a0aaa791731cbbf5f397000d5baed8ce
VkMishra <vineetmishra2451@gmail.com>
2020-05-01 00:41:35 +0530

    Added Doubling Time of cases with graph

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ $.getJSON(
         chartDeceased(data.cases_time_series);
         chartCases(data.cases_time_series);
         chartRecovered(data.cases_time_series);
+        doublingRate(data.cases_time_series);
     });    
 function objectToArray(data){
   data.forEach(function(s,i){
@@ -43,14 +44,80 @@ var chart = new google.visualization.BubbleChart(document.getElementById('series
 chart.draw(data, options);
 
 }
-
 /*******************Line Charts******************/
+var dRateArray = [];
 var testingTotal = [];
 var testingPositive = [];
 var recoveredArray = [];
 var casesArray = [];
 var deceasedArray = [];
 
+/**************Doubling Rate Calculator**********/
+function doublingRate(data) {
+  var sum = 0;
+  for (let i = data.length-1, n = 1; n <= 7; i--, n++) {
+    sum += parseInt(data[i].dailyconfirmed);
+  }
+  var avg = sum / 7;
+  var perc = (avg * 100) / parseInt(data[data.length - 1].totalconfirmed);
+  var dRate = 72 / perc; //using rule of 72
+  dRate = dRate.toFixed(2);
+  var currentCases =parseInt(data[data.length - 1].totalconfirmed) ;
+  var day = document.querySelector("#days");
+  day.innerHTML=dRate + '<br>Days'; 
+  for (let i = 0; i <= parseInt(dRate)+1; i++) {
+    let arr;
+    if (i == 0) {
+      arr = [i, currentCases];
+      dRateArray.push(arr);
+    }
+    else {
+      currentCases += currentCases * 0.05;
+      arr = [i, currentCases];
+      dRateArray.push(arr);
+    }
+  }
+}
+//Chart
+google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(dRateChart);
+
+    function dRateChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Cases');
+
+      data.addRows(dRateArray);
+
+      var options = {
+        colors:'red',
+        backgroundColor : 'none',
+        hAxis: {
+          textStyle: {
+              color: "#ffa34d",
+              fontName: "sans-serif",
+              fontSize: 10,
+              bold: false,
+              italic: false
+          }
+      },
+      vAxis: {
+        textStyle: {
+            color: "#ffa34d",
+            fontName: "sans-serif",
+            fontSize: 10,
+            bold: false,
+            italic: false
+        }
+    },
+    colors: '#ffa34d',
+    };
+
+      var chart = new google.charts.Line(document.getElementById('dRateChart'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
 
 /******************Testings***************/
 function chartTesting(data){
