c50d385a20a70ee892180e4dc1a6aaac1480a3eb
VkMishra <vineetmishra2451@gmail.com>
2020-05-01 23:18:14 +0530

    Added icons and changed css a little

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var dataToArray = [["State",'Recovered','Deaths','Active Cases','Total Cases']];
 $.getJSON(
     'https://api.covid19india.org/data.json', 
     function(data) {
-        objectToArray(data.statewise); 
+        objectToArray(data.statewise);
+        updateCounter(data.cases_time_series); 
         chartTesting(data.tested);
         chartDeceased(data.cases_time_series);
         chartCases(data.cases_time_series);
@@ -34,11 +35,14 @@ var data = new google.visualization.arrayToDataTable(dataToArray);
 var options = {
   title: 'Number of Deaths, Recovered Patients, Active Cases ' +
          'and Total Cases of Indian States (Live)',
-  hAxis: {title: 'Recovered'},
-  vAxis: {title: 'Died'},
+  hAxis: {title: 'Recovered',
+          gridlines :{color:'#aacfcf'}},
+  vAxis: {title: 'Died',
+  gridlines :{color:'#ffb6b6'}},
   colorAxis :{colors:['#0c9463','#ffd31d','#e32249']},
   backgroundColor : 'none',
-  bubble: {textStyle: {fontSize: 12}}      };
+  bubble: {textStyle: {fontSize: 10,bold :true}},  
+ };
 
 var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_div'));
 chart.draw(data, options);
@@ -64,7 +68,11 @@ function doublingRate(data) {
   dRate = dRate.toFixed(2);
   var currentCases =parseInt(data[data.length - 1].totalconfirmed) ;
   var day = document.querySelector("#days");
-  day.innerHTML=dRate + '<br>Days'; 
+  day.innerHTML='<span class="fa-layers fa-fw" style="background:white">'+
+                '<i class="fad fa-calendar">'+
+                '</i><span class="fa-layers-text" data-fa-transform="shrink-12 down-3" style="font-weight:900">' + dRate + 
+                '</span></span>';                
+  
   for (let i = 0; i <= parseInt(dRate)+1; i++) {
     let arr;
     if (i == 0) {
@@ -93,25 +101,26 @@ google.charts.load('current', {'packages':['line']});
       var options = {
         colors:'red',
         backgroundColor : 'none',
-        hAxis: {
+        hAxis: {         
           textStyle: {
-              color: "#ffa34d",
+              color: "#bb3b0e",
               fontName: "sans-serif",
-              fontSize: 10,
+              fontSize: 12,
               bold: false,
               italic: false
-          }
+          },
+          
       },
       vAxis: {
         textStyle: {
-            color: "#ffa34d",
+            color: "#bb3b0e",
             fontName: "sans-serif",
-            fontSize: 10,
+            fontSize: 12,
             bold: false,
             italic: false
         }
     },
-    colors: '#ffa34d',
+    colors: '#bb3b0e',
     };
 
       var chart = new google.charts.Line(document.getElementById('dRateChart'));
@@ -404,7 +413,7 @@ google.charts.load('current', {'packages':['line']});
 
 function total(data){
   var totalCases = document.querySelector("#total")
-  totalCases.textContent = data.confirmed
+  totalCases.textContent = data.confirmed 
 
   var activeCases = document.querySelector("#active")
   activeCases.textContent = data.active
@@ -414,4 +423,16 @@ function total(data){
 
   var deaths = document.querySelector("#deaths")
   deaths.textContent = data.deaths
+}
+
+function updateCounter(data){
+  var update = data[data.length-1];
+  var newDeaths = document.querySelector("#plusDeaths")
+  var newRecovered = document.querySelector("#plusRecovered")
+  var newActive = document.querySelector("#plusActive")
+  var newTotal = document.querySelector("#plusTotal")
+
+  newTotal.innerHTML = '<i class="fad fa-chevron-up totalUp"></i>  ' + update.dailyconfirmed;
+  newRecovered.innerHTML = '<i class="fad fa-chevron-up recoveredUp"></i>  ' +  update.dailyrecovered;
+  newDeaths.innerHTML = '<i class="fad fa-chevron-up deathsUp"></i>  ' + update.dailydeceased;
 }
